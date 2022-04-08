b131d2f65252e6d2f40da598bf66adaf1e420fa7
Nell254 <91744221+Nell254@users.noreply.github.com>
2022-04-08 15:53:07 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let x = 32
+if (x == 9){
+  console.log("True");  
+} else {console.log("False");}
+
+
+let y = "Green"
+if (y == "Blue") {
+  console.log("Blue Detected!");
+} else if (y == "Green"){
+ console.log("Green Detetced!"); 
+} else {console.log("No green or blue detected!");}
